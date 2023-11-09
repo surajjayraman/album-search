9d8b1d3f27dfd1af075213eaaf299cfc6ddaff96
surajjayraman <suraj.jayraman@gmail.com>
2023-11-08 20:11:56 -0500

    Add cypress get form group to checkbox.spec

--- react-albums/cypress/e2e/checkbox.spec.cy.js
+++ react-albums/cypress/e2e/checkbox.spec.cy.js
@@ -2,6 +2,8 @@
 describe('tests to see if cypress is working', () => {
 
     it('can visit the web page', () => {
-        cy.visit('http://localhost:8002/')
+        cy.visit('http://localhost:8002/');
+
+        cy.get('.filters__form-group')
     });
 })
